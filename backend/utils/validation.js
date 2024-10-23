da2d1075615fb567688d6d46d77679aed7b6844c
Andrew <andrew.lizon12@gmail.com>
2024-10-22 17:09:30 -0700

    Refactor error messages and the authorization helper func

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -3,7 +3,7 @@ const { validationResult } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
-const handleValidationErrors = (req, _res, next) => {
+const handleValidationErrors = (req, res, next) => {
     const validationErrors = validationResult(req);
   
     if (!validationErrors.isEmpty()) { 
@@ -17,6 +17,13 @@ const handleValidationErrors = (req, _res, next) => {
       err.status = 400;
       err.title = "Bad request.";
       next(err);
+      // res.status(400).json({
+      //   "message": "Bad Request",
+      //   "errors": {
+      //     "credential": "Email or username is required",
+      //     "password": "Password is required"
+      //   }
+      // })
     }
     next();
   };
