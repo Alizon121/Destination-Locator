585a752ec1bc10e4ce6e8637ba1a5ab79dfe864a
Andrew <andrew.lizon12@gmail.com>
2024-10-25 20:54:54 -0700

    Remove console.log in delete a reivew

--- backend/routes/api/reviewImages.js
+++ backend/routes/api/reviewImages.js
@@ -19,10 +19,9 @@ router.delete('/:reviewImageId', requireAuth, async (req, res, next) => {
             include: {model: Review, attributes: ['userId']}
         });
 
-        console.log(findingImageId)
         if (!findingImageId) return res.status(404).json({ message: "Review Image couldn't be found"})
         if (user !== findingImageId.Review.userId ) res.status(403).json({message: "Forbidden"})
-            
+
         await findingImageId.destroy();
         res.json({ message: "Successfully deleted" })
     } catch(error) {
