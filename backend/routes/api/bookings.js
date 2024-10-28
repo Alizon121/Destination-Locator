cb43f598537a3763a0707a76b426434a395a1665
Erika <brandonerika96@gmail.com>
2024-10-28 18:31:48 -0400

    Booking model file was imported to the Booking route instance

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {Spot, SpotImage, Review, ReviewImage, User} = require("../../db/models");
+const {Spot, SpotImage, Review, ReviewImage, User, Booking} = require("../../db/models");
 const { Model, json } = require('sequelize');
 const { requireAuth } = require('../../utils/auth');
 const { parse } = require('dotenv');
