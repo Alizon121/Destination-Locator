f138821a72434896fc6e5d89480de10e0603eb40
Erika <brandonerika96@gmail.com>
2024-10-28 21:28:12 -0400

    get current user bookings completed

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -8,7 +8,45 @@ const { parse } = require('dotenv');
 
 
 /***************************GET CURRENT USER'S BOOKINGS*****************************/
+router.get('/current', requireAuth, async (req, res, next) => {
+    try {
+        const currentId = req.user.id;
+        const bookings = await Booking.findAll({
+            where: { userId: currentId},
+            include: {
+                model: Spot,
+                as: 'Spot',
+                attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
+                include: {
+                    model: SpotImage,
+                    attributes: ['url'],
+                    where: { preview: true },
+                    required: false
+                }
+            }
+        })
 
+        const newFormat = bookings.map(booking => ({
+            id: booking.id,
+            spotId: booking.spotId,
+            Spot: {
+                ...booking.Spot.get(), //get() Sequelize method retrieves values of model as plain JS object
+                previewImage: booking.Spot.SpotImages.length > 0 ? booking.Spot.SpotImages[0].url : null
+            },
+            userId: booking.userId,
+            startDate: booking.startDate,
+            endDate: booking.endDate,
+            createdAt: booking.createdAt,
+            updatedAt: booking.updatedAt
+    }));
+
+    res.json({Bookings: newFormat})
+
+    } catch (error) {
+        next(error);
+    }
+
+});
 
 
 /***************************GET BOOKINGS BASED ON SPOT ID*****************************/
