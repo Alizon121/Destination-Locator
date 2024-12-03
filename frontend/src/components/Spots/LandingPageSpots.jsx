84a8867e77bac72e9ca954f47801db6770a43838
Andrew <andrew.lizon12@gmail.com>
2024-12-02 19:55:18 -0800

    Style the landing page

--- frontend/src/components/Spots/LandingPageSpots.jsx
+++ frontend/src/components/Spots/LandingPageSpots.jsx
@@ -1,33 +1,36 @@
 import { useEffect } from 'react'
 import { loadSpotsData } from '../../store/spots';
 import { useDispatch, useSelector } from 'react-redux';
-
+import './LandingPageSpots.css'
 function LandingPageSpots() {
     // Local state variable for obtaining the spots
     const dispatch = useDispatch();
     const spots = useSelector(state => state.spots)
-    const spotsData = Object.values(spots).map(spot =>spots[spot.id])
+    const spotsData = Object.values(spots).map(spot => spots[spot.id])
 
     useEffect(() => {
         dispatch(loadSpotsData())
     }, [dispatch])
 
     return (
-        <div>   
+        <div className='spot_grid'>
             {spotsData.map((spot) => {
                 return (
-                  <>
-                        <img src={spot.previewImage}/>
-                    <ul>
-                        <li>{spot.city}</li>,
-                        <li>{spot.state}</li>
-                        <li>{`$${spot.price}.00/night`}</li>
-                        <li>★{spot.avgRating}</li>
-                    </ul>
-                  </>
+                    <>
+                        <div key={spot.id} className='spot_item'> {/* Add a key and a wrapper for each spot */}
+                            <img className="spot_image" src={spot.previewImage} alt="Spot Preview" />
+                            <span>
+                                <ul className='spots_data'>
+                                    <li className='spot_city_state'>{`${spot.city}, ${spot.state}`}</li>
+                                    <li className='spot_price'>{`$${spot.price}.00/night`}</li>
+                                    <li className='spot_rating'>★{spot.avgRating}</li>
+                                </ul>
+                            </span>
+                        </div>
+                    </>
                 )
             })
-            }        
+            }
         </div>
     )
 }
